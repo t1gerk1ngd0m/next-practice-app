85d8dfe61ef8ae9e9907cb126630dcf679e81b40
Taiga Miwa <taiga.miwa888@gmail.com>
2021-03-22 18:23:14 +0900

    Layout調整

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,4 @@
 import { FC } from 'react'
-import 'tailwindcss/tailwind.css'
-import Link from 'next/link'
-import Home from '../../pages/index'
-import Tasks from '../../pages/tasks'
 
 const Layout: FC = ({ children }) => (
   <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
