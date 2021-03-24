15c719b7f892203b883a22bc0216cff26d72a276
Taiga Miwa <taiga.miwa888@gmail.com>
2021-03-24 17:41:43 +0900

    タスクform作成

--- pages/tasks/index.tsx
+++ pages/tasks/index.tsx
@@ -4,7 +4,23 @@ const Tasks: FC = (props: any) => (
   <div className="container mx-auto px-6 py-8">
     <h3 className="text-gray-700 text-3xl font-medium">Tasks</h3>
     <div className="mt-4">
-      <h4 className="text-gray-600">Simple Table</h4>
+      <div className="p-6 bg-white rounded-md shadow-md">
+        <form>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mt-4">
+            <div>
+              <label className="text-gray-700">タスク名</label>
+              <input className="form-input w-full mt-2 rounded-md focus:border-indigo-600" type="text"/>
+            </div>
+            <div>
+              <label className="text-gray-700">メモ</label>
+              <input className="form-input w-full mt-2 rounded-md focus:border-indigo-600" type="email"/>
+            </div>
+          </div>
+          <div className="flex justify-end mt-4">
+            <button className="px-4 py-2 bg-gray-800 text-gray-200 rounded-md hover:bg-gray-700 focus:outline-none focus:bg-gray-700">Save</button>
+          </div>
+        </form>
+      </div>
       <div className="mt-6">
         <div className="bg-white shadow rounded-md overflow-hidden my-6">
           <table className="text-left w-full border-collapse">
